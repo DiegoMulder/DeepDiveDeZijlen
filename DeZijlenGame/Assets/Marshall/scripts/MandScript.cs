079063f35a69f667b0199469bcdc3d5f5228a50d
marshall verboekend <marshalllyron@gmail.com>
2024-11-19 14:08:03 +0100

    sorteergame fix

--- DeZijlenGame/Assets/Marshall/scripts/MandScript.cs
+++ DeZijlenGame/Assets/Marshall/scripts/MandScript.cs
@@ -6,6 +6,10 @@ public class MandController : MonoBehaviour
 {
     public string juisteCubeTag;
 
+    public Transform redTeleportLocation;
+    public Transform blueTeleportLocation;
+    public Transform greenTeleportLocation;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(juisteCubeTag))
@@ -15,6 +19,27 @@ public class MandController : MonoBehaviour
         else
         {
             Debug.Log("Fout! " + other.tag + "hoort niet in " + gameObject.tag);
+            Transform teleportLocation = GetTeleportLocation(other.tag);
+
+            if (teleportLocation != null)
+            {
+                other.transform.position = teleportLocation.position;
+            }
+        }
+    }
+
+    private Transform GetTeleportLocation(string tag)
+    {
+        switch (tag)
+        {
+            case "RedCube":
+                return redTeleportLocation;
+            case "BlueCube":
+                return blueTeleportLocation;
+            case "GreenCube":
+                return greenTeleportLocation;
+            default:
+                return null;
         }
     }
 }
